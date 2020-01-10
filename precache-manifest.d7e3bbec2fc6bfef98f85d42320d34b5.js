0212f4ad3e41421af3d1235be8f258299a7445fe
Yanuwar Ishak <yanuwarishak@gmail.com>
2020-01-10 15:53:35 +0700

    Updates

---
+++ precache-manifest.d7e3bbec2fc6bfef98f85d42320d34b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79d930eb28eb467f18460f309cf2cf2",
+    "url": "yanuwarishak.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "440b2a36f825cde3d8f4",
+    "url": "yanuwarishak.github.io/monsters-rolodex/static/css/main.6fa95b5b.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "yanuwarishak.github.io/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "yanuwarishak.github.io/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "440b2a36f825cde3d8f4",
+    "url": "yanuwarishak.github.io/monsters-rolodex/static/js/main.67e3aefe.chunk.js"
+  },
+  {
+    "revision": "e1d413998239d9ca0090",
+    "url": "yanuwarishak.github.io/monsters-rolodex/static/js/runtime-main.ec4cb957.js"
+  }
+]);
